f8933fbf2e71721809ef0586dc23aaffb380fbf6
DojanDev <272136@unifio.edu.br>
2025-09-09 12:09:39 +0000

    atualizaçãoRouter - 09/09

--- nome-projeto/src/app/app.ts
+++ nome-projeto/src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,7 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('projeto-integrador');
+  //Metodo construtor objeto router no template, isso é feito injetando o Router no componente e tornando-o público, assim o template pode acessar router.url
+  constructor(public router: Router) {}
 }
 
